6a32a813dc3e1fab193ed63f155cfe8cc2c9f270
GuoyJeng <guojing2549@gmail.com>
2025-10-03 12:05:36 +0700

    Update

---
+++ script.js
@@ -0,0 +1,14 @@
+const myinput = document.getElementById("myinput")
+
+function add() {
+    myinput.value = Number(myinput.value) + 1
+}
+
+function decrease() {
+    if (Number(mynumber)) {
+        myinput.value = Number(myinput.value) - 0
+    }
+    else {
+        myinput.value = Number(myinput.value) - 1
+    }
+}
